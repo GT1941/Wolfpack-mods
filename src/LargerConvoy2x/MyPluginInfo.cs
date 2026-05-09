e6e9cae64c6e981bd84a595860ad6fe67fb200d0
GT <xyberx@gmail.com>
2026-05-09 11:57:16 +0200

    Rename LargerConvoy -> LargerConvoy2x to match variant naming

---
+++ src/LargerConvoy2x/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LargerConvoy2x
+{
+    internal static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "LargerConvoy2x";
+        public const string PLUGIN_NAME = "LargerConvoy2x";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
