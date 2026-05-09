ab5b10691e63a03da1f261bd170259978d683721
GT <xyberx@gmail.com>
2026-05-09 13:48:55 +0200

    LogbookSeconds 1.1.1: append torpedo metadata to the launch line in-place (no extra blank line)

--- src/LogbookSeconds/MyPluginInfo.cs
+++ src/LogbookSeconds/MyPluginInfo.cs
@@ -4,7 +4,7 @@ namespace LogbookSeconds
     {
         public const string PLUGIN_GUID = "LogbookSeconds";
         public const string PLUGIN_NAME = "LogbookSeconds";
-        public const string PLUGIN_VERSION = "1.1.0";
+        public const string PLUGIN_VERSION = "1.1.1";
         public const string PLUGIN_REPO = "https://github.com/GT1941/Wolfpack-mods";
     }
 }
