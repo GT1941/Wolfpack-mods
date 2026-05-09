4a4c9e6c3fdbe0a7048ce4e93cf1e25aac0f8438
GT <xyberx@gmail.com>
2026-05-09 11:49:55 +0200

    Add LogbookSeconds: in-game logbook HH:MM -> HH:MM:SS upgrade

---
+++ src/LogbookSeconds/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LogbookSeconds
+{
+    internal static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "LogbookSeconds";
+        public const string PLUGIN_NAME = "LogbookSeconds";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
