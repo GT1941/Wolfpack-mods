6c56d3f9a763727d25ea86e9ecdaa47fda6af8ed
GT <xyberx@gmail.com>
2026-05-09 12:14:14 +0200

    Add PLUGIN_REPO constant pointing at GT1941/Wolfpack-mods

--- src/GameTime/MyPluginInfo.cs
+++ src/GameTime/MyPluginInfo.cs
@@ -5,5 +5,6 @@ namespace GameTime
         public const string PLUGIN_GUID = "GameTime";
         public const string PLUGIN_NAME = "GameTime";
         public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_REPO = "https://github.com/GT1941/Wolfpack-mods";
     }
 }
