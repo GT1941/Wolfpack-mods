8cf3d3df3c84bf4c7486ae695031917d085476a4
GT <xyberx@gmail.com>
2026-05-09 12:14:15 +0200

    Add PLUGIN_REPO constant pointing at GT1941/Wolfpack-mods

--- src/LargerConvoy1_5x/MyPluginInfo.cs
+++ src/LargerConvoy1_5x/MyPluginInfo.cs
@@ -5,5 +5,6 @@ namespace LargerConvoy1_5x
         public const string PLUGIN_GUID = "LargerConvoy1_5x";
         public const string PLUGIN_NAME = "LargerConvoy1.5x";
         public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_REPO = "https://github.com/GT1941/Wolfpack-mods";
     }
 }
