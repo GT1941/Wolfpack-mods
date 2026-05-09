998822b664a7ba0f51f584826cb89d13dead62c0
GT <xyberx@gmail.com>
2026-05-09 12:14:17 +0200

    Add PLUGIN_REPO constant pointing at GT1941/Wolfpack-mods

--- src/LogbookExport/MyPluginInfo.cs
+++ src/LogbookExport/MyPluginInfo.cs
@@ -5,5 +5,6 @@ namespace LogbookExport
         public const string PLUGIN_GUID = "LogbookExport";
         public const string PLUGIN_NAME = "LogbookExport";
         public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_REPO = "https://github.com/GT1941/Wolfpack-mods";
     }
 }
