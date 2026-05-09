05fd7a6f40899b8af6d4a35eff98fa46f7b6d15f
GT <xyberx@gmail.com>
2026-05-09 11:49:57 +0200

    Add LargerConvoy1.5x: gentler convoy scaling variant

---
+++ src/LargerConvoy1_5x/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LargerConvoy1_5x
+{
+    internal static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "LargerConvoy1_5x";
+        public const string PLUGIN_NAME = "LargerConvoy1.5x";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
